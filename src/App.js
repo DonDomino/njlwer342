ba51cc6151b4ca50793ed97a354da333c6a15f7a
Mauricio Orozco <maudob@live.com>
2019-07-20 20:18:26 -0500

    Running tests

--- src/App.js
+++ src/App.js
@@ -3,18 +3,42 @@ import React, { Component } from 'react';
 /// Modifica el componente para que se puedan agregar tareas
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      text : "",
+      tasks : ["Sacar la ropa", "Hacer la cama", "Leer un rato"]
+    }
+    this.trackInput = this.trackInput.bind(this);
+    this.addTask = this.addTask.bind(this)
+  }
+
+  trackInput(event){
+    this.setState({
+      text : event.target.value
+    })
+  }
+
+  addTask(event){
+    event.preventDefault();
+    this.setState({
+      tasks : this.state.tasks.concat(this.state.text),
+      text : ""
+    })
+  }
+
   render() {
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            <li>Sacar la ropa</li>
-            <li>Hacer la cama</li>
-            <li>Leer un rato</li>
+            {this.state.tasks.map(task =>
+              <li>{task}</li>
+            )}
           </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
+           <form onSubmit={this.addTask}>
+             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" value={this.state.text} onChange={this.trackInput}/>
            </form>
         </div>
       </div>
